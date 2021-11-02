cfd78e24594cb53edfa55c8ed93530c13d47463d
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-02 14:29:02 -0300

    ForEach

--- server.js
+++ server.js
@@ -7,7 +7,35 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("pages/index",)
+  const items = [
+    {
+      title: "D",
+      message: "Desenvolver aplicações/serviços de forma fácil"
+    },
+    {
+      title: "E",
+      message: "EJS usa JavaScript para renderizar HTML"
+    },
+    {
+      title: "M",
+      message: "Muito fácil de usar"
+    },
+    {
+      title: "A",
+      message: "Amorzinho"
+    },
+    {
+      title: "I",
+      message: "Install EJS"
+    },
+    {
+      title: "S",
+      message: "Sintaxe simples"
+    }
+  ];
+  res.render("pages/index",{
+    qualitys: items
+  })
 })
 
 app.get("/sobre", (req, res) => {
